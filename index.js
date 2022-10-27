f0a56bb9959bd1a1b346246fd6e6f4a5a50bf282
saurabhrana7699 <1304rishabhrana@gmail.com>
2022-10-27 19:53:31 +0530

    newupload

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
-// let menu = document.querySelector('#menu-bars');
-// let header = document.querySelector('header');
+let menu = document.querySelector('#menu-bars');
+let header = document.querySelector('header');
 
-// menu.onclick = () =>{
-//     menu.classList.toggle('fa-times');
-//     header.classList.toggle('active');
-// }
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
 
-// window.onscroll = () =>{
-//     menu.classList.remove('fa-times');
-//     header.classList.remove('active');
-// }
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+//  back to top JS ************************************************
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+//  End***********************************************************************
 
 // let cursor1 = document.querySelector('.cursor-1');
 // let cursor2 = document.querySelector('.cursor-2');
