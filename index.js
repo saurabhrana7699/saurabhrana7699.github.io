559d3d5cf29ef2584aa382c9a9245aa3630aed1d
saurabhrana7699 <1304rishabhrana@gmail.com>
2022-10-28 01:06:07 +0530

    newapply

--- index.js
+++ index.js
@@ -42,27 +42,27 @@ function backToTop() {
 
 //  End***********************************************************************
 
-// let cursor1 = document.querySelector('.cursor-1');
-// let cursor2 = document.querySelector('.cursor-2');
+let cursor1 = document.querySelector('.cursor-1');
+let cursor2 = document.querySelector('.cursor-2');
 
-// window.onmousemove = (e) =>{
-//     console.log("working....")
-//     cursor1.style.top = e.pageY + 'px';
-//     cursor1.style.left = e.pageX + 'px';
-//     cursor2.style.top = e.pageY + 'px';
-//     cursor2.style.left = e.pageX + 'px';
-// }
+window.onmousemove = (e) =>{
+    console.log("working....")
+    cursor1.style.top = e.pageY + 'px';
+    cursor1.style.left = e.pageX + 'px';
+    cursor2.style.top = e.pageY + 'px';
+    cursor2.style.left = e.pageX + 'px';
+}
 
-// document.querySelectorAll('a').forEach(links =>{
+document.querySelectorAll('a').forEach(links =>{
 
-//     links.onmouseenter = () =>{
-//         cursor1.classList.add('active');
-//         cursor2.classList.add('active');
-//     }
+    links.onmouseenter = () =>{
+        cursor1.classList.add('active');
+        cursor2.classList.add('active');
+    }
 
-//     links.onmouseleave = () =>{
-//         cursor1.classList.remove('active');
-//         cursor2.classList.remove('active');
-//     }
+    links.onmouseleave = () =>{
+        cursor1.classList.remove('active');
+        cursor2.classList.remove('active');
+    }
 
-// });
+});
